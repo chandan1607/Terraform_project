130c36b946ad0a6642f38bc468534068ae1f5192
Chandan <71363385+chandan1607@users.noreply.github.com>
2026-08-30 16:03:13 +0530

    dev_and staging

---
+++ dev/main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rgname
+  location = var.location
+}
+
+module "ServicePrincipal" {
+  source                 = "../modules/ServicePrincipal"
+  service_principal_name = var.service_principal_name
+}
+
+resource "azurerm_role_assignment" "rolespn" {
+  scope                = "/subscriptions/${var.SUB_ID}"
+  role_definition_name = "Contributor"
+  principal_id         = module.ServicePrincipal.service_principal_object_id
+
+  skip_service_principal_aad_check = true
+}
+
+module "keyvault" {
+  source = "../modules/keyvault"
+
+  keyvault_name               = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = var.rgname
+  service_principal_name      = var.service_principal_name
+  service_principal_object_id = module.ServicePrincipal.service_principal_object_id
+  service_principal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
+}
+
+resource "azurerm_key_vault_secret" "sp_secret" {
+  name         = "${var.service_principal_name}-client-secret"
+  value        = module.ServicePrincipal.client_secret
+  key_vault_id = module.keyvault.keyvault_id
+}
+
+module "aks" {
+  source                 = "../modules/aks"
+  service_principal_name = var.service_principal_name
+  client_id              = module.ServicePrincipal.client_id
+  client_secret          = module.ServicePrincipal.client_secret
+  location               = var.location
+  resource_group_name    = var.rgname
+
+  cluster_name   = var.cluster_name
+  node_pool_name = var.node_pool_name
+}
+
+resource "local_file" "kubeconfig" {
+  filename = "${path.root}/kubeconfig"
+  content  = module.aks.config
+}
