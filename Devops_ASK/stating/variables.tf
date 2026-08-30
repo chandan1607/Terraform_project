a0deae71fab3c2e81579edbe9bc6508a212a98c6
Chandan <71363385+chandan1607@users.noreply.github.com>
2026-08-30 16:05:09 +0530

    dev and stating

---
+++ Devops_ASK/stating/variables.tf
@@ -0,0 +1,25 @@
+variable "rganame" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+variable "location" {
+  type    = string
+  default = "canadacentral"
+}
+variable "service_principal_name" {
+  type = string
+}
+
+variable "keyvault_name" {
+  type = string
+}
+
+variable "SUB_ID" {
+  type = string
+}
+variable "node_pool_name" {
+  
+}
+variable "cluster_name" {}
+  
